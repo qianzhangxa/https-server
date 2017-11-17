d1dd0578ef136e26851d8bfc9200481367ab5a73
Qian Zhang <zhq527725@gmail.com>
2017-11-16 18:32:00 -0800

    Made the HTTPS server support both IPv4 and IPv6.

--- https_server.py
+++ https_server.py
@@ -1,12 +1,15 @@
-import BaseHTTPServer
-import SimpleHTTPServer
+import socket
 import ssl
 import sys
+import SimpleHTTPServer
+from BaseHTTPServer import HTTPServer
 
+class HTTPServerV6(HTTPServer):
+  address_family = socket.AF_INET6
 
 if __name__ == "__main__":
-  httpd = BaseHTTPServer.HTTPServer(
-    ('127.0.0.1', int(sys.argv[1])),
+  httpd = HTTPServerV6(
+    ('', int(sys.argv[1])),
     SimpleHTTPServer.SimpleHTTPRequestHandler)
 
   httpd.socket = ssl.wrap_socket(
@@ -15,3 +18,4 @@ if __name__ == "__main__":
     server_side=True)
 
   httpd.serve_forever()
+
